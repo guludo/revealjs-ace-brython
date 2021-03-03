357e34afc5d4b8b53a8c1d376b64c33b79ab01b1
Gustavo Sousa <gustavo.jo.sousa@gmail.com>
2021-03-03 02:35:30 -0300

    index: use CodeTree instead of CodeNode

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import ace from 'ace-builds'
 import 'ace-builds/src-noconflict/mode-python.js'
 
-import { CodeNode } from './code-tree.js'
+import CodeTree from './code-tree.js'
 
 
 export default {
@@ -38,23 +38,61 @@ class Plugin {
   }
 
   handleReady() {
+    this.createCodeTree()
     this.editorContainers = [...this.deck.getRevealElement().querySelectorAll(this.config.selector)]
     for (let c of this.editorContainers) {
       this.configureEditor(c)
     }
   }
 
-  configureEditor(container) {
-    const scriptElement = container.querySelector(':scope > script[type="text/ace-brython"]')
-    let code;
-    if (scriptElement) {
-      code = scriptElement.textContent;
-    } else {
-      code = container.textContent;
-    }
+  createCodeTree() {
+    const allScripts = [...this.deck.getRevealElement().querySelectorAll('script[type="text/x.ace-brython"]')]
 
-    const codeNode = new CodeNode(null, code)
-    container.textContent = codeNode.code
+    const scripts = allScripts.filter(s => !('ref' in s.dataset && s.dataset.ref))
+    const refs = allScripts.filter(s => 'ref' in s.dataset && s.dataset.ref)
+
+    const idToScript = new Map()
+
+    // Create nodes configuration objects
+    const nodes = scripts.map(s => {
+      const n = {}
+      if ('id' in s.dataset && s.dataset.id) {
+        n.id = s.dataset.id
+      }
+
+      if ('parent' in s.dataset && s.dataset.parent) {
+        n.parent = s.dataset.parent
+      }
+      n.code = s.textContent
+      return n
+    })
+
+    // Construct the tree
+    this.codeTree = new CodeTree(nodes)
+
+    // Bind code nodes to script elements
+    this.scriptToCodeNode = new WeakMap()
+    for (let i = 0; i < scripts.length; i++) {
+      const s = scripts[i]
+      const node = this.codeTree.nodes[i]
+      this.scriptToCodeNode.set(s, node)
+    }
+    for (let s of refs) {
+      const id = s.dataset.ref
+      if (!this.codeTree.nodeMap.has(id)) {
+        throw new Error(`invalid reference to script "${id}"`)
+      }
+      this.scriptToCodeNode.set(s, this.codeTree.nodeMap.get(id))
+    }
+  }
+
+  configureEditor(container) {
+    const scriptElement = container.querySelector(':scope > script[type="text/x.ace-brython"]')
+    if (!scriptElement) {
+      return
+    }
+    const node = this.scriptToCodeNode.get(scriptElement)
+    container.textContent = node.code
 
     ace.edit(container, {
       mode: 'ace/mode/python',
