985f34d00556d61dfca3bf8b68c2bd748c15f1d9
Gustavo Sousa <gustavo.jo.sousa@gmail.com>
2021-03-04 18:39:13 -0300

    editor: use template!

--- src/editor.js
+++ src/editor.js
@@ -1,22 +1,36 @@
 import ace from 'ace-builds'
 import 'ace-builds/src-noconflict/mode-python.js'
 
+import EditorTemplateHtml from './editor-template.html';
+
+const EditorTemplate = document.createElement('template')
+EditorTemplate.innerHTML = EditorTemplateHtml
 
 class Editor {
   constructor(container, codeNode) {
     this.codeNode = codeNode
 
-    this.aceEditor = ace.edit(container, {
+    this.handleCodeNodeChange = (code) => {
+      this.aceEditor.setValue(code)
+    }
+    this.codeNode.addCodeChangeCallback(this.handleCodeNodeChange)
+
+    this.container = container
+    const shadow = this.container.attachShadow({mode: 'open'})
+    shadow.appendChild(EditorTemplate.content.cloneNode(true))
+
+    this.aceElement = this.container.appendChild(document.createElement('div'))
+    this.aceElement.slot = 'editor'
+    this.aceElement.style.fontSize = '1em'
+    this.aceElement.style.width = '100%'
+    this.aceElement.style.height = '100%'
+    this.aceElement.textContent = this.codeNode.code
+    this.aceEditor = ace.edit(this.aceElement, {
       mode: 'ace/mode/python',
     })
     this.aceEditor.on('change', () => {
       this.codeNode.setCode(this.aceEditor.getValue(), this.handleCodeNodeChange)
     })
-
-    this.handleCodeNodeChange = (code) => {
-      this.aceEditor.setValue(code)
-    }
-    this.codeNode.addCodeChangeCallback(this.handleCodeNodeChange)
   }
 
   destroy() {
