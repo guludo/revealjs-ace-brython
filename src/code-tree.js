9d356ad69591fb94a71a9e3e3ab94b6f4aee1557
Gustavo Sousa <gustavo.jo.sousa@gmail.com>
2021-03-06 13:21:38 -0300

    preprocessor: move preprocessing logic to separate module

--- src/code-tree.js
+++ src/code-tree.js
@@ -1,3 +1,6 @@
+import preprocess from './preprocessor.js'
+
+
 class CodeTree {
   constructor(nodes) {
     this.nodeMap = new Map()
@@ -148,56 +151,4 @@ class CodeNode {
   removeCodeChangeCallback(callback) {
     this.codeChangeCallbacks.delete(callback)
   }
-}
-
-
-function preprocess(code) {
-  let [lines, firstLine] = trimBlankLines(code.split(/\r?\n/))
-
-  const padding = lines.length ? /^\s*/.exec(lines[0])[0] : ''
-
-  const r = []
-
-  // Remove padding from the lines
-  let lineno = firstLine + 1
-  for (let line of lines) {
-    lineno++
-
-    if (!line.length) {
-      r.push('')
-      continue
-    }
-
-    if (!line.startsWith(padding)) {
-      throw new Error(`preprocess: line ${lineno} missing padding starting line`)
-    }
-    line = line.slice(padding.length)
-    r.push(line)
-  }
-
-  return r.join('\n')
-}
-
-
-function trimBlankLines(lines) {
-  // Ignore first blank lines
-  let firstLine = 0
-  while (firstLine < lines.length) {
-    const line = lines[firstLine]
-    if (/\S/.test(lines[firstLine])) {
-      break
-    }
-    firstLine++
-  }
-
-  // Ignore last blank lines
-  let lastLine = lines.length - 1
-  while (lastLine >= 0) {
-    if (/\S/.test(lines[lastLine])) {
-      break
-    }
-    lastLine--
-  }
-
-  return [lines.slice(firstLine, lastLine + 1), firstLine]
 }
